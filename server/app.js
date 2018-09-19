ab089e5e416c0b42fd4a7e0aa4fd2d7e0c943a50
Brian Ko <brian.kostar@gmail.com>
2018-09-18 21:22:07 -0400

    format

--- server/app.js
+++ server/app.js
@@ -1,29 +1,28 @@
-const http = require('http');
-const express = require('express')
-const logger = require('morgan')
-const bodyParser = require('body-parser')
-const cors = require('cors')
-const helmet = require('helmet')
+const http = require("http");
+const express = require("express");
+const logger = require("morgan");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const helmet = require("helmet");
 
-require('./child')
+require("./child");
 
 const app = express();
-const router = express.Router()
+const router = express.Router();
 
 // app.use(logger);
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({extended:false}))
-app.use(cors())   //prevent cors request err
-app.use(helmet()) //armors our api against attacks
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cors()); //prevent cors request err
+app.use(helmet()); //armors our api against attacks
 
 const port = parseInt(process.env.PORT, 10) || 8000;
 
 // router.route('/')
-app.get('/', function (req, res) {
-    res.send('Hello World!')
-  })
-
-app.listen(port, () => {
-    console.log(`Server started at port: ${port}`);
+app.get("/", function(req, res) {
+  res.send("Hello World!");
 });
 
+app.listen(port, () => {
+  console.log(`Server started at port: ${port}`);
+});
